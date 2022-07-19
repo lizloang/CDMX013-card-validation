6cfae09aba48da43c26dfeb0eb3b84ff8ff60f8e
lizloang <liz.loang@gmail.com>
2022-07-19 18:08:32 -0500

    ultimos ajustes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+
+/*function hide() {
+  let number = document.getElementById("fnumber");
+  number.value = validator.maskify(number.value);
+}*/
+
+document.getElementById("fnumber").addEventListener("blur", hide);
+
+function hide(event) {
+  let creditCardNumber = event.target.value;
+
+  companyImg(creditCardNumber);
+
+  event.target.type = "text";
+  event.target.value = validator.maskify(creditCardNumber);
+  if (validator.isValid(creditCardNumber) === true) {
+    document.getElementById("isValid").style = "display:block";
+    document.getElementById("isValid").innerHTML = "Número de tarjeta válido";
+    //aqui poner un icono mejor
+  } else {
+    document.getElementById("isValid").style = "display:block";
+    document.getElementById("isValid").innerHTML =
+      "Número de tarjeta no es válido";
+  }
+}
+
+function companyImg(creditCardNumber) {
+  if (validator.company(creditCardNumber) === true) {
+    console.log(validator.company(creditCardNumber));
+    document.getElementById("company").innerHTML =
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/1/16/Former_Visa_%28company%29_logo.svg">';
+  } else {
+    console.log(validator.company(creditCardNumber));
+    document.getElementById("company").innerHTML =
+      '<img src="https://logodownload.org/wp-content/uploads/2014/07/mastercard-logo-1.png">';
+    document.getElementById("company").style = "display:block";
+  }
+}
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,49 @@
 const validator = {
   // ...
+  maskify: function (creditCardNumber) {
+    let numberMasked;
+    if (creditCardNumber.length > 4) {
+      numberMasked =
+        "#".repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
+
+      return numberMasked;
+    } else {
+      return creditCardNumber;
+    }
+  },
+
+  isValid: function (creditCardNumber) {
+    let numberInProcess = [];
+    let sumaDigitos = 0;
+    for (let i = 0, l = creditCardNumber.length; i < l; i++) {
+      if ((i + 1) % 2 === 0) {
+        if (parseInt(creditCardNumber[i]) * 2 > 9) {
+          numberInProcess.push(parseInt(creditCardNumber[i]) * 2 - 9);
+        } else {
+          numberInProcess.push(parseInt(creditCardNumber[i] * 2));
+        }
+      } else {
+        numberInProcess.push(parseInt(creditCardNumber[i]));
+      }
+      sumaDigitos = sumaDigitos + numberInProcess[i];
+    }
+
+    if (sumaDigitos % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  company: function (creditCardNumber) {
+    if (parseInt(creditCardNumber[0]) === 4) {
+      console.log(creditCardNumber[0]);
+      return true;
+    } else if (parseInt(creditCardNumber[0]) === 5) {
+      console.log(creditCardNumber[0]);
+      return false;
+    }
+  },
 };
 
 export default validator;
